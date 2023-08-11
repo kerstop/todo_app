0f27cfac2b7abfa9ba0e9c6ad742ce8c5f328318
kerstop <packetson2@gmail.com>
2023-08-10 23:43:24 -0500

    added sorting to entries

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,8 @@ import { LoginBar } from "./LoginBar";
 
 function App() {
   const [list, setList] = React.useState<TodoListEntry[]>([]);
-  const [user, setUser] = React.useState<string| null>(null);
-  const description_field = React.useRef<null| HTMLInputElement>(null)
+  const [user, setUser] = React.useState<string | null>(null);
+  const description_field = React.useRef<null | HTMLInputElement>(null);
 
   React.useEffect(() => {
     fetch(`/api/todo`)
@@ -14,35 +14,38 @@ function App() {
         console.log(val.body);
         return val.json();
       })
-      .then((val) => {
-        setList(val.todoEntries);
-        setUser(val.username)
+      .then((val: {username: string, todoEntries: TodoListEntry[]}) => {
+        setList(val.todoEntries.sort((a,b)=>a.id - b.id));
+        setUser(val.username);
       });
   }, []);
 
   const createTodo: React.FormEventHandler = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     if (description_field.current !== null) {
-      const description = description_field.current.value
+      const description = description_field.current.value;
       if (description === null) return;
-      console.log(description)
+      console.log(description);
       fetch("/api/todo", {
         body: JSON.stringify({
           description: description,
         }),
-        method: "POST"
+        method: "POST",
       })
-      .then((resp) => {
-        return resp.json()
-      }).then((resp: number) => {
-
-        setList([...list, {
-          id: resp,
-          description: description,
-          complete: false
-        }])
-      })
+        .then((resp) => {
+          return resp.json();
+        })
+        .then((resp: number) => {
+          setList([
+            ...list,
+            {
+              id: resp,
+              description: description,
+              complete: false,
+            },
+          ]);
+        });
     }
   };
 
@@ -51,7 +54,13 @@ function App() {
       <LoginBar username={user}></LoginBar>
       <TodoList listEntries={list} setListEntries={setList}></TodoList>
       <form onSubmit={createTodo}>
-        <input ref={description_field} type="text" name="description" placeholder="Todo" required></input>
+        <input
+          ref={description_field}
+          type="text"
+          name="description"
+          placeholder="Todo"
+          required
+        ></input>
         <input type="button" value="Submit"></input>
       </form>
     </>
